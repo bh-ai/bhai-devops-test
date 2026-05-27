e9738fe4b3ea1a8aa0c779334a3ff179f1abbe3c
bh-sathish-git <sathish@bighammer.ai>
2026-05-27 19:37:09 +0530

    Update spark_job_pipeline_526.py

--- flows/bh_project_id=299/flow/flow_id=526/dags/spark_job_pipeline_526.py
+++ flows/bh_project_id=299/flow/flow_id=526/dags/spark_job_pipeline_526.py
@@ -80,6 +80,8 @@ with DAG(
         - "/app/tmp/schemas/"
       sparkVersion: "3.5.0"
       sparkConf:
+        "spark.kubernetes.driverEnv.AZURE_VAULT_URL": "https://bh-dev-westus3-kv.vault.azure.net/"
+        "spark.executorEnv.AZURE_VAULT_URL": "https://bh-dev-westus3-kv.vault.azure.net/"
         "spark.gluten.sql.columnar.backend.velox.glogSeverityLevel": "1"
         "spark.gluten.sql.columnar.backend.velox.glogVerboseLevel": "1"
         "spark.eventLog.compress": "true"
