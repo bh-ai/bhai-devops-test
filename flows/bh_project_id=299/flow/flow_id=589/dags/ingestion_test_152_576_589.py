45ffa69b97350d23617e60955792dcc7ed752ed9
bh-sathish-git <sathish@bighammer.ai>
2026-06-16 12:27:43 +0530

    Publish medallion pipeline

--- flows/bh_project_id=299/flow/flow_id=589/dags/ingestion_test_152_576_589.py
+++ flows/bh_project_id=299/flow/flow_id=589/dags/ingestion_test_152_576_589.py
@@ -802,7 +802,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_ingestion_test_152_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1058/ingestion_test_152.json",
+                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1058/ingestion_test_152.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
@@ -974,7 +974,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_silver_raw_fidelis_ny_downstate_enrollment_to_members_dedupe_load_260615_5583_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1059/silver_raw_fidelis_ny_downstate_enrollment_to_members_dedupe_load_260615_5583.json",
+                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1059/silver_raw_fidelis_ny_downstate_enrollment_to_members_dedupe_load_260615_5583.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
