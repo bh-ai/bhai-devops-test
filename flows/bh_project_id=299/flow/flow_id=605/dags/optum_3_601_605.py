8f9483d833471817fc734811edea678726de499e
bh-varadha-git <varadha@bighammer.ai>
2026-06-22 12:37:23 +0530

    Deploy flow 605

--- flows/bh_project_id=299/flow/flow_id=605/dags/optum_3_601_605.py
+++ flows/bh_project_id=299/flow/flow_id=605/dags/optum_3_601_605.py
@@ -807,7 +807,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_optum_3_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1098/optum_3.json",
+                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1098/optum_3.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
