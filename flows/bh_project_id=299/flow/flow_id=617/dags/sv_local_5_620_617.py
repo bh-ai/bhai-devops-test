44916920173b2c9bdff4ec188fd0f85bdb6aef7c
bh-varadha-git <varadha@bighammer.ai>
2026-06-26 11:58:20 +0530

    Publish medallion pipeline

--- flows/bh_project_id=299/flow/flow_id=617/dags/sv_local_5_620_617.py
+++ flows/bh_project_id=299/flow/flow_id=617/dags/sv_local_5_620_617.py
@@ -332,7 +332,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_sv_local_5_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1115/sv_local_5.json",
+                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1115/sv_local_5.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
@@ -511,7 +511,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_silver_raw_sv_local_5_member_enrollment_load_260626_7bc6_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1116/silver_raw_sv_local_5_member_enrollment_load_260626_7bc6.json",
+                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1116/silver_raw_sv_local_5_member_enrollment_load_260626_7bc6.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
