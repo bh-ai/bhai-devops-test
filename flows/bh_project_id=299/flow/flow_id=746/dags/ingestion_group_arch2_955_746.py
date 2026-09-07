196d333699103b7f7eb82044846aaf00399a72ba
bh-sathish-git <sathish@bighammer.ai>
2026-09-07 15:54:16 +0530

    Publish medallion pipeline

--- flows/bh_project_id=299/flow/flow_id=746/dags/ingestion_group_arch2_955_746.py
+++ flows/bh_project_id=299/flow/flow_id=746/dags/ingestion_group_arch2_955_746.py
@@ -131,7 +131,7 @@ with DAG(
             "name": "{{ dag.dag_id }}_run_pipelines_ingestion_group_arch2_{{ ts_nodash }}",
             "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
             "parameters": [
-                "/Workspace/Shared/codespace/test/pipelines/bh_project_id=299/pipeline/pipeline_id=1705/ingestion_group_arch2.json",
+                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1705/ingestion_group_arch2.json",
                 "databricks",
                 "/Workspace/Shared/dev-utils/schemas"
             ]
