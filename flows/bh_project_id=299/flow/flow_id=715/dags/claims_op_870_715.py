03feb8ba956ee55484c6edc9809f5949d9fd5081
bh-sathish-git <sathish@bighammer.ai>
2026-09-22 17:18:18 +0530

    Publish medallion pipeline

--- flows/bh_project_id=299/flow/flow_id=715/dags/claims_op_870_715.py
+++ flows/bh_project_id=299/flow/flow_id=715/dags/claims_op_870_715.py
@@ -248,57 +248,11 @@ with DAG(
         "batch_control": "{{ ti.xcom_pull(task_ids='validate_inbound_files', key='batch_control') }}",
         "pool_enabled": True,
         "pool_heartbeat_interval_seconds": 120,
-        "pool_release_lease": False
+        "pool_release_lease": True
     }
     run_pipelines_pharmacy_silver_ods_updated = PythonOperator(
         pre_execute=common_task.pre_execute_callback,
         task_id='run_pipelines_pharmacy_silver_ods_updated',
-        python_callable=submit_job_to_cluster,
-        params=_submit_params,
-        on_success_callback=feed_control_callbacks.submit_job_success_callback,
-        on_failure_callback=feed_control_callbacks.submit_job_failure_callback,
-    )
-
-    from airflow.operators.python import PythonOperator
-    from airflow_plugins.compute_pool.databricks_tasks import submit_job_to_cluster
-
-    _submit_params = {
-        "compute_task_id": "create_compute",
-        "job_config": {
-            "job_type": "spark_python",
-            "name": "{{ dag.dag_id }}_run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4_{{ ts_nodash }}",
-            "python_file": "/Workspace/Shared/dev-utils/pipelines/main.py",
-            "parameters": [
-                "/Workspace/Shared/codespace/pipelines/bh_project_id=299/pipeline/pipeline_id=1505/silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4.json",
-                "databricks",
-                "/Workspace/Shared/dev-utils/schemas"
-            ]
-        },
-        "ingestion_group_id": 870,
-        "flow_id": 715,
-        "pipeline_id": "1505",
-        "feed_name": "claims_op",
-        "validate_inbound_task_id": "validate_inbound_files",
-        "facts_source": "databricks",
-        "pipeline_name": "silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4",
-        "run_data_quality_rules": False,
-        "dq_rules_source": "git",
-        "airflow_connection_id": "databricks_default",
-        "pipeline_key": "silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4",
-        "bh_project_id": 299,
-        "project_id": 299,
-        "project_name": "flow-test-project",
-        "compute_xcom_key": "return_value",
-        "valid_files": "{{ task_instance.xcom_pull(task_ids='validate_inbound_files', key='valid_files') }}",
-        "batch_id": "{{ task_instance.xcom_pull(task_ids='validate_inbound_files', key='batch_id') }}",
-        "batch_control": "{{ ti.xcom_pull(task_ids='validate_inbound_files', key='batch_control') }}",
-        "pool_enabled": True,
-        "pool_heartbeat_interval_seconds": 120,
-        "pool_release_lease": True
-    }
-    run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4 = PythonOperator(
-        pre_execute=common_task.pre_execute_callback,
-        task_id='run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4',
         python_callable=submit_job_to_cluster,
         params=_submit_params,
         on_success_callback=feed_control_callbacks.submit_job_success_callback,
@@ -568,9 +522,7 @@ with DAG(
     create_compute >> run_pipelines_claims_silver_ods_fidelis_upstate_ip
     run_pipelines_claims_silver_ods_fidelis_upstate_ip >> run_pipelines_pharmacy_silver_ods_updated
     create_compute >> run_pipelines_pharmacy_silver_ods_updated
-    run_pipelines_pharmacy_silver_ods_updated >> run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4
-    create_compute >> run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4
-    run_pipelines_silver_raw_fideliscare_claims_to_pharmacy_pipeline_260804_18b4 >> archive_processed_files
+    run_pipelines_pharmacy_silver_ods_updated >> archive_processed_files
     archive_processed_files >> delete_compute
     create_compute >> delete_compute
     delete_compute >> end_flow_task
